340d25e0911bd2a09bb2de321ce17adf3c439990
andreea-tamasan <91469946+andreea-tamasan@users.noreply.github.com>
2021-10-06 08:40:29 +0300

    Add files via upload
    Am implementat interfata cu utilizatorul.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def is_palindrome(n):
     while cn != 0:
         pal = pal * 10 + cn % 10
         cn = cn // 10
-    if (n == pal) :
+    if (n == pal):
         return True
     else:
         return False
@@ -40,7 +40,7 @@ def is_superprime(n):
             for i in range(2, n // 2 + 1):
                 if n % i == 0:
                     return False
-            n = n//10
+            n = n // 10
     return True
 
 
@@ -54,25 +54,77 @@ def test_is_superprime():
     assert is_superprime(1) is False
 
 
-while True:
-    print("1. Determinati palindromul unui numar")
-    print("2. Determinati daca un numar este superprim")
-    print("x. Iesire")
-
-    optiune = input("Dati optiunea: ")
-    if optiune == "1":
-        n = int(input("Dati numarul pentru care doriti sa vedeti daca e palindrom: "))
-        if is_palindrome(n) :
-            print (" este palindrom")
-        else:
-            print (" nu este palindrom")
-    elif optiune == "2":
-        n = int(input("Dati numarul pentru care doriti sa vedeti daca e superprim: "))
-        if is_superprime(n):
-            print(" este superprim")
-        else:
-            print(" nu este superprim")
-    elif optiune == "x":
-        break
+def is_prime(n):
+    """
+    Determina daca un numar dat este prim
+    :param n: nr. intreg
+    :return: True daca acesta este numar prim, False in caz contrar
+    """
+    if n > 1:
+        for i in range(2, n):
+            if n % i == 0:
+                return False
     else:
-        print("Optiune gresita. Incercati din nou!")
+        return False
+    return True
+
+
+def get_largest_prime_below(n):
+    """
+    Calculeaza ultimul nr. prim mai mic decat un nr. dat
+    :param n: nr. intreg
+    :return: numarul prim mai mic decat nr. dat
+    """
+    for i in reversed(range(n)):
+        if is_prime(i):
+            return i
+
+
+def test_get_largest_prime_below():
+    """
+    Testeaza daca functia get_largest_prime_below(n) functioneaza
+    :return: true daca nr. e superprim, false in caz contrar
+    """
+    assert get_largest_prime_below(4) == 3
+    assert get_largest_prime_below(5) == 3
+    assert get_largest_prime_below(11) == 7
+    assert get_largest_prime_below(14) == 13
+
+
+def main():
+    test_is_palindrome()
+    test_is_superprime()
+    test_get_largest_prime_below()
+    while True:
+        print("1. Determinati palindromul unui numar")
+        print("2. Determinati daca un numar este superprim")
+        print("3. Determinati ultimul nr. prim mai mic decat un nr. dat")
+        print("x. Iesire")
+
+        optiune = input("Dati optiunea: ")
+        if optiune == "1":
+            n = int(input("Dati numarul pentru care doriti sa vedeti daca e palindrom: "))
+            if is_palindrome(n):
+                print(" este palindrom")
+            else:
+                print(" nu este palindrom")
+        elif optiune == "2":
+            n = int(input("Dati numarul pentru care doriti sa vedeti daca e superprim: "))
+            if is_superprime(n):
+                print(" este superprim")
+            else:
+                print(" nu este superprim")
+        elif optiune == "3":
+            n = int(input("Dati numar: "))
+            if get_largest_prime_below(n):
+                print(get_largest_prime_below(n))
+            else:
+                print("Nu exista nr. prim mai mic ca n")
+        elif optiune == "x":
+            break
+        else:
+            print("Optiune gresita. Incercati din nou!")
+
+
+if __name__ == '__main__':
+    main()
